fbd9101f22b0dd524496cf635f89c2c1139cca04
Connor Rhodes <connor@rhodes.contact>
2024-10-19 16:08:59 +0000

    add logging to check_maestral_working.rb

--- check_maestral_working.rb
+++ check_maestral_working.rb
@@ -1,7 +1,14 @@
+def write_log(message)
+  log_path = '/home/cron/.local/share/cron_logs/check_maestral_working.log'
+  puts message
+  log_message = "#{Time.now.to_i}: #{message}\n"
+  File.open(log_path, 'a') {|f| f.write log_message}
+end
+
 output = `maestral status`
 
 if output == "Maestral daemon is not running.\n"
-  puts "Maestral daemon is not running."
+  write_log "Maestral daemon is not running."
   exit(1)
 end
 
@@ -14,7 +21,7 @@ error_line = output[1]
 error_count = error_line.match(/Sync errors\s+(\d+)/)[1].to_i
 
 if error_count > 0
-  puts "Sync Errors"
+  write_log "Sync Errors"
   exit(1)
 end
 
@@ -22,15 +29,15 @@ status_line = output[0].rstrip
 status = status_line.split(' ', 2)[1]
 
 if status == "Paused"
-  puts "Paused"
+  write_log "Paused"
   exit(1)
 end
 
 if status == "Up to date"
-  puts "Up to date"
+  write_log "Up to date"
   `curl -s https://hc-ping.com/4923c31a-6605-4612-9758-30d546a82100`
   exit(0)
 end
 
-puts "case not caught"
+write_log "case not caught"
 exit(1)
